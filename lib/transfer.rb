d8fa03c384c36a55491cd79eeff0a3c3ef9522a1
Kevin Mathias <komathias1@gmail.com>
2020-04-09 03:24:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,26 @@ class Transfer
   attr_accessor :balance
   
   def initialize(transfer, sender, receiver)
+    
   end
+  
+  def sender
+    
+  end
+  
+  def receiver
+    
+  end
+  
+  def transfer
+    
+  end
+  
+  
+  
+  
+  
+  
+  
   
 end
